894c0bc92d69f057ca29a0ba005cd15e720982d0
Petyo Kostakov <petyo_kostakov@starsgroup.com>
2022-01-07 13:02:20 +0200

    First commit

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        String myFirstName = "Petyo";
+
+        System.out.format("Hello, %s!", myFirstName);
+        System.out.println();
+        System.out.println(Integer.MAX_VALUE);
+    }
+
+}
