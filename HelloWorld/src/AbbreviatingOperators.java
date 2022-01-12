0adbfe9c16cd84b0dcaee277da4981f94ac361f8
Petyo Kostakov <petyo_kostakov@starsgroup.com>
2022-01-12 10:36:15 +0200

    Abbreviating operators

---
+++ HelloWorld/src/AbbreviatingOperators.java
@@ -0,0 +1,16 @@
+public class AbbreviatingOperators {
+    public static void main(String[] args) {
+        // % - remainder operator
+        int result = 0;
+
+        result++; // 0 + 1 = 1
+        System.out.println(result); // 1
+
+        result--;
+        System.out.println(result); // 0
+
+        result += 2; // 0 + 2 = 2
+        System.out.println(result);
+
+    }
+}
---
+++ HelloWorld/src/CharactersAndBooleans.java
@@ -0,0 +1,15 @@
+public class CharactersAndBooleans {
+    public static void main(String[] args) {
+
+        char myUnicodeChar = '\u0044';
+        System.out.println(myUnicodeChar);
+
+        boolean trueBooleanValue = true;
+        int customerAge = 25;
+        boolean isCustomerOver21 = customerAge > 21;
+        System.out.println(isCustomerOver21);
+
+        String stringAndInt = "123" + 456;
+        System.out.println(stringAndInt);
+    }
+}
